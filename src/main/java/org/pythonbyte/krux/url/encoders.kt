b1a180d56ba9054cc62d83342ffe02945bc51a75
Mark Nenadov <mark.nenadov@borderconnect.com>
2018-12-13 20:45:55 -0500

    added encoders.mapToUrlParameterString

--- src/main/java/org/pythonbyte/krux/url/encoders.kt
+++ src/main/java/org/pythonbyte/krux/url/encoders.kt
@@ -2,6 +2,10 @@ package org.pythonbyte.krux.url
 
 import java.net.URLEncoder
 
-fun utf8Url( value: String ): String? {
-    return URLEncoder.encode( value, "UTF-8")
+fun utf8Url(value: String): String? {
+    return URLEncoder.encode(value, "UTF-8")
+}
+
+fun mapToUrlParameterString(parameterMap: AbstractMap<String, String>) : String {
+    return parameterMap.keys.map { parameterName -> parameterName + "=" + parameterMap.get(parameterName)}.joinToString( separator = "&" )
 }
