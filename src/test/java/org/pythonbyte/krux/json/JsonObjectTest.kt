832fedad5e0ac8c909411e5db529304b35a9e7df
Mark Nenadov <marknenadov@gmail.com>
2023-10-22 07:45:45 -0400

    fill in some more tests for JsonObject

--- src/test/java/org/pythonbyte/krux/json/JsonObjectTest.kt
+++ src/test/java/org/pythonbyte/krux/json/JsonObjectTest.kt
@@ -1,11 +1,36 @@
 package org.pythonbyte.krux.json
 
 import org.json.JSONObject
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class JsonObjectTest {
+    @Test
+    fun testHasKey() {
+        val jsonObject = JsonObject( JSONObject( "{\"name\":\"Elizabeth\"}" ))
+
+        assertTrue(jsonObject.hasKey("name"))
+        assertFalse(jsonObject.hasKey("age"))
+    }
+
+    @Test
+    fun testGetString() {
+        val jsonObject = JsonObject( JSONObject( "{\"name\":\"Elizabeth\"}" ))
+
+        val result = jsonObject.getString("name")
+
+        assertEquals("Elizabeth", result)
+    }
+
+    @Test
+    fun testGetInt() {
+        val jsonObject = JsonObject( JSONObject( "{\"age\":33}" ))
+
+        val result = jsonObject.getInt("age")
+
+        assertEquals(33, result)
+    }
+
     @Test
     fun testGetStringArray() {
         val jsonObject = JsonObject( JSONObject( "{\"names\": [\"mark\", \"bob\"]}" ))
