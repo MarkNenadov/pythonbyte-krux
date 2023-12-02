460e600278398c95bab868d0f72df8e6fe05876a
Mark Nenadov <marknenadov@gmail.com>
2023-12-02 16:36:17 -0500

    redo 1.2 and fix byte buffer extension

--- src/main/java/org/pythonbyte/krux/string/ByteBufferExtension.kt
+++ src/main/java/org/pythonbyte/krux/string/ByteBufferExtension.kt
@@ -2,7 +2,8 @@ package org.pythonbyte.krux.string
 
 import java.nio.ByteBuffer
 
-fun ByteBuffer.asString(): ByteBuffer {
+fun ByteBuffer.asString(): String {
     val bytes = ByteArray(this.remaining())
-    return this.get(bytes)
+    this.get(bytes)
+    return String(bytes, Charsets.UTF_8)
 }
