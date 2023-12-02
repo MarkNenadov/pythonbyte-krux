398f3e16abfe61c52673cc2de4693025f62fe05b
Mark Nenadov <marknenadov@gmail.com>
2023-12-02 16:28:28 -0500

    ..

--- src/main/java/org/pythonbyte/krux/string/ByteBufferExtension.kt
+++ src/main/java/org/pythonbyte/krux/string/ByteBufferExtension.kt
@@ -2,7 +2,7 @@ package org.pythonbyte.krux.string
 
 import java.nio.ByteBuffer
 
-fun ByteBuffer.toString(): ByteBuffer {
+fun ByteBuffer.asString(): ByteBuffer {
     val bytes = ByteArray(this.remaining())
     return this.get(bytes)
 }
