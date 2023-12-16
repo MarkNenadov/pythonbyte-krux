76dc2268908d088652b471465e077b2724e63397
Mark Nenadov <marknenadov@gmail.com>
2023-12-16 06:08:26 -0500

    Implement String.gunzip and rewrite String.gzip. With test cases.

--- src/main/java/org/pythonbyte/krux/conversions/ConversionUtils.kt
+++ src/main/java/org/pythonbyte/krux/conversions/ConversionUtils.kt
@@ -1,7 +1,11 @@
 package org.pythonbyte.krux.conversions
 
+import java.io.BufferedReader
+import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
+import java.io.InputStreamReader
 import java.nio.ByteBuffer
+import java.util.zip.GZIPInputStream
 import java.util.zip.GZIPOutputStream
 import kotlin.io.encoding.Base64
 
@@ -14,11 +18,28 @@ fun ByteBuffer.asString(): String {
 fun String.gzip(): String {
     val byteArrayOutputStream = ByteArrayOutputStream()
 
-    GZIPOutputStream(byteArrayOutputStream).bufferedWriter(Charsets.UTF_8).use {
-        it.write(this)
+    GZIPOutputStream(byteArrayOutputStream).use {
+        it.write(this.toByteArray(Charsets.UTF_8))
     }
 
-    return String(byteArrayOutputStream.toByteArray())
+    return byteArrayOutputStream.toByteArray().toString(Charsets.ISO_8859_1)
+}
+
+fun String.gunzip(): String {
+    val compressedData = this.toByteArray(Charsets.ISO_8859_1)
+    val inputStream = ByteArrayInputStream(compressedData)
+    val reader = BufferedReader(InputStreamReader(GZIPInputStream(inputStream), Charsets.UTF_8))
+
+    val result = StringBuilder()
+    var line: String?
+
+    reader.use {
+        while (reader.readLine().also { line = it } != null) {
+            result.append(line)
+        }
+    }
+
+    return result.toString()
 }
 
 @kotlin.io.encoding.ExperimentalEncodingApi
---
+++ src/test/java/org/pythonbyte/krux/conversions/ConversionUtilsTest.kt
@@ -0,0 +1,31 @@
+package org.pythonbyte.krux.conversions
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import kotlin.io.encoding.ExperimentalEncodingApi
+
+class ConversionUtilsTest {
+    @OptIn(ExperimentalEncodingApi::class)
+    @Test
+    fun testGzipAndBase64() {
+        val expectedBase64 = "H++/vQgAAAAAAADvv73vv71NLO+/vQYAT11G77+9BAAAAA=="
+        assertEquals(
+            expectedBase64,
+            "Mark".gzip().base64(),
+        )
+    }
+
+    @OptIn(ExperimentalEncodingApi::class)
+    @Test
+    fun testGzipAndGunzip() {
+        val value = "My name is Ed. I am a turtle."
+        val expectedGzippedBase64Value =
+            "H8KLCAAAAAAAAMO/w7PCrVTDiEvDjE1Vw4gsVnBNw5FTw7BUSMOMVUhUKCktKsOJScOVAwBLw47DuDgdAAAA"
+
+        assertEquals(
+            expectedGzippedBase64Value,
+            value.gzip().base64(),
+        )
+        assertEquals(value, value.gzip().gunzip())
+    }
+}
