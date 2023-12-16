bab92777675e03139af9ef4f2944dd85f9d4f02d
Mark Nenadov <marknenadov@gmail.com>
2023-12-16 06:13:20 -0500

    fix wildcard import

--- src/test/java/org/pythonbyte/krux/conversions/ConversionUtilsTest.kt
+++ src/test/java/org/pythonbyte/krux/conversions/ConversionUtilsTest.kt
@@ -1,6 +1,6 @@
 package org.pythonbyte.krux.conversions
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import kotlin.io.encoding.ExperimentalEncodingApi
 
