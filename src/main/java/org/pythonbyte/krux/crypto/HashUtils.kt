4c9503276de660afa3533d20fbc7492491f6bfe9
Mark Nenadov <marknenadov@gmail.com>
2024-03-23 20:16:52 -0400

    sha-256 support

--- src/main/java/org/pythonbyte/krux/crypto/HashUtils.kt
+++ src/main/java/org/pythonbyte/krux/crypto/HashUtils.kt
@@ -1,22 +1,23 @@
-package org.pythonbyte.krux.crypto
+package org.pythonbyxte.krux.crypto
 
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 
 object HashUtils {
-    private const val HASH_ALGORITHM = "SHA-1"
+    private const val SHA1_HASH_ALGORITHM = "SHA-1"
+    private const val SHA256_HASH_ALGORITHM = "SHA-256"
 
-    fun String.sha1(): String {
+    fun String.sha1(): String = getDigest(this, SHA1_HASH_ALGORITHM).toHexString()
+
+    fun String.sha256(): String = getDigest(this, SHA256_HASH_ALGORITHM).toHexString()
+
+    private fun getDigest(value: String, algorithm: String): ByteArray {
         return try {
-            getSha1Digest(this).toHexString()
-        } catch (e: NoSuchAlgorithmException) {
-            throw RuntimeException("Error creating SHA-1 Digest", e)
+            val messageDigest = MessageDigest.getInstance(algorithm)
+            messageDigest.digest(value.toByteArray())
+        }  catch (e: NoSuchAlgorithmException) {
+            throw RuntimeException("Error creating $algorithm Digest", e)
         }
-    }
-
-    private fun getSha1Digest(value: String): ByteArray {
-        val messageDigest = MessageDigest.getInstance(HASH_ALGORITHM)
-        return messageDigest.digest(value.toByteArray())
     }
 
     private fun ByteArray.toHexString(): String {
--- src/test/java/org/pythonbyte/krux/crypto/HashUtilsTest.kt
+++ src/test/java/org/pythonbyte/krux/crypto/HashUtilsTest.kt
@@ -3,12 +3,20 @@ package org.pythonbyte.krux.crypto
 import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
-import org.pythonbyte.krux.crypto.HashUtils.sha1
+import org.pythonbyxte.krux.crypto.HashUtils.sha1
+import org.pythonbyxte.krux.crypto.HashUtils.sha256
 
 class HashUtilsTest {
     @Test
     fun testSha1() {
         val result = "Mark".sha1()
+        assertNotNull(result)
+        assertNotEquals("", result)
+    }
+
+    @Test
+    fun testSha256() {
+        val result = "Mark".sha256()
         assertNotNull(result)
         assertNotEquals("", result)
     }
