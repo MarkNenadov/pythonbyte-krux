05fd8a0f9896dc9f60baaa302a5bbb2988397c69
Mark Nenadov <marknenadov@gmail.com>
2024-03-23 20:37:36 -0400

    refactor HashUtils implementations and add String.hash() which supports custom MessageDigest

--- src/main/java/org/pythonbyte/krux/crypto/HashUtils.kt
+++ src/main/java/org/pythonbyte/krux/crypto/HashUtils.kt
@@ -1,5 +1,7 @@
 package org.pythonbyxte.krux.crypto
 
+import org.pythonbyxte.krux.crypto.HashUtils.sha1
+import org.pythonbyxte.krux.crypto.HashUtils.toHexString
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 
@@ -7,17 +9,15 @@ object HashUtils {
     private const val SHA1_HASH_ALGORITHM = "SHA-1"
     private const val SHA256_HASH_ALGORITHM = "SHA-256"
 
-    fun String.sha1(): String = getDigest(this, SHA1_HASH_ALGORITHM).toHexString()
+    fun String.sha1(): String = this.hash(getDigest(SHA1_HASH_ALGORITHM))
 
-    fun String.sha256(): String = getDigest(this, SHA256_HASH_ALGORITHM).toHexString()
+    fun String.sha256(): String = this.hash(getDigest(SHA256_HASH_ALGORITHM))
 
-    private fun getDigest(
-        value: String,
-        algorithm: String,
-    ): ByteArray {
+    fun String.hash(messageDigest: MessageDigest): String = messageDigest.digest(this.toByteArray()).toHexString()
+
+    private fun getDigest(algorithm: String): MessageDigest {
         return try {
-            val messageDigest = MessageDigest.getInstance(algorithm)
-            messageDigest.digest(value.toByteArray())
+            MessageDigest.getInstance(algorithm)
         } catch (e: NoSuchAlgorithmException) {
             throw RuntimeException("Error creating $algorithm Digest", e)
         }
