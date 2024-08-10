dd22cc2d4a8242c4efeffeacd39e39da08b1e6f4
Mark Nenadov <marknenadov@gmail.com>
2024-08-09 22:15:00 -0400

    add JsonObject.randomKey

--- src/main/java/org/pythonbyte/krux/json/JsonObject.kt
+++ src/main/java/org/pythonbyte/krux/json/JsonObject.kt
@@ -3,30 +3,30 @@ package org.pythonbyte.krux.json
 import org.json.JSONObject
 import java.math.BigDecimal
 
-class JsonObject(val wrappedJSONObject: JSONObject) {
+class JsonObject(
+    val wrappedJSONObject: JSONObject,
+) {
     constructor(jsonString: String) : this(JSONObject(jsonString))
 
-    fun getString(key: String): String {
-        return if (isNull(key)) {
+    fun getString(key: String): String =
+        if (isNull(key)) {
             ""
         } else {
             wrappedJSONObject.getString(key)
         }
-    }
 
     fun getInt(key: String): Int = wrappedJSONObject.getInt(key)
 
     fun getBigDecimal(key: String): BigDecimal = wrappedJSONObject.getBigDecimal(key)
 
-    fun getArray(key: String): List<JsonObject> {
-        return if (hasKey(key)) {
+    fun getArray(key: String): List<JsonObject> =
+        if (hasKey(key)) {
             wrappedJSONObject.getJSONArray(key).map {
                 JsonObject(it as JSONObject)
             }
         } else {
             emptyList()
         }
-    }
 
     fun getStringArray(key: String): List<String> {
         return if (hasKey(key)) {
@@ -34,7 +34,7 @@ class JsonObject(val wrappedJSONObject: JSONObject) {
                 it.toString()
             }
         } else {
-            return listOf()
+            return emptyList()
         }
     }
 
@@ -42,11 +42,15 @@ class JsonObject(val wrappedJSONObject: JSONObject) {
 
     fun getObject(key: String): JsonObject = JsonObject(wrappedJSONObject.getJSONObject(key))
 
-    fun hasKey(key: String): Boolean = wrappedJSONObject.has(key)
-
     override fun toString(): String = wrappedJSONObject.toString()
 
     fun getBoolean(key: String): Boolean = wrappedJSONObject.getBoolean(key)
 
+    // key operations
+
+    fun hasKey(key: String): Boolean = wrappedJSONObject.has(key)
+
     fun getKeys(): List<String> = wrappedJSONObject.keys().asSequence().toList()
+
+    fun randomKey(): String = getKeys().random()
 }
