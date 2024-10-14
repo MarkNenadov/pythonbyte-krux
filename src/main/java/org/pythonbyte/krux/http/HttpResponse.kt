00caab93d54b0b23a099d15f87e164e2717f7bcb
Mark Nenadov <marknenadov@gmail.com>
2024-10-14 07:20:21 -0400

    add getJsonArray to HttpResponse

--- src/main/java/org/pythonbyte/krux/http/HttpResponse.kt
+++ src/main/java/org/pythonbyte/krux/http/HttpResponse.kt
@@ -2,10 +2,13 @@ package org.pythonbyte.krux.http
 
 import com.squareup.okhttp.Response
 import com.squareup.okhttp.ResponseBody
+import org.json.JSONArray
 import org.json.JSONObject
 import org.pythonbyte.krux.json.JsonObject
 
-class HttpResponse(private val response: Response) {
+class HttpResponse(
+    private val response: Response,
+) {
     fun isOk(): Boolean = response.code() == 200
 
     fun getBytes(): ByteArray = getBody().bytes()
@@ -15,6 +18,12 @@ class HttpResponse(private val response: Response) {
     fun getJsonObject(): JsonObject {
         val responseBody = getBody().string().trim()
         return JsonObject(JSONObject(responseBody))
+    }
+
+    fun getJsonArray(): List<JsonObject> {
+        val responseBody = getBody().string().trim()
+        val jsonObjects = JSONArray(responseBody).toList()
+        return jsonObjects.map { JsonObject(it as JSONObject) }
     }
 
     private fun getBody(): ResponseBody = response.body()
