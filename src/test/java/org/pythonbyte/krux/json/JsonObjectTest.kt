f84a14bb2107d935ef315a1fc54fc14a2b59f343
Mark Nenadov <marknenadov@gmail.com>
2025-03-23 21:00:54 -0400

    missing coverage in JsonObjectTest

--- src/test/java/org/pythonbyte/krux/json/JsonObjectTest.kt
+++ src/test/java/org/pythonbyte/krux/json/JsonObjectTest.kt
@@ -5,23 +5,53 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import java.math.BigDecimal
 
 class JsonObjectTest {
     @Test
     fun testHasKey() {
-        val jsonObject = JsonObject(JSONObject("{\"name\":\"Elizabeth\"}"))
-
-        assertTrue(jsonObject.hasKey("name"))
-        assertFalse(jsonObject.hasKey("age"))
+        with(JsonObject(JSONObject("{\"name\":\"Elizabeth\"}"))) {
+            assertTrue(hasKey("name"))
+            assertFalse(hasKey("age"))
+        }
     }
 
     @Test
-    fun testGetString() {
+    fun testRandomKey() {
+        with(JsonObject(JSONObject("{\"name\":\"Elizabeth\", \"age\": 4, \"place\": \"Detroit\"}"))) {
+            assertTrue(listOf("name", "age", "place").contains(randomKey()))
+        }
+    }
+
+    @Test
+    fun testGetStrings() {
+        val jsonObject = JsonObject(JSONObject("{\"name\":\"Elizabeth\", \"age\": 4, \"place\": \"Detroit\"}"))
+
+        with(jsonObject.getStrings("name", "place")) {
+            assertTrue(contains("Elizabeth"))
+            assertTrue(contains("Detroit"))
+        }
+    }
+
+    @Test
+    fun testGetStringWhenAbsent() {
+        with(JsonObject(JSONObject("{\"name\":\"Elizabeth\"}"))) {
+            assertEquals("", getString("place"))
+        }
+    }
+
+    @Test
+    fun testGetStringWhenExists() {
         val jsonObject = JsonObject(JSONObject("{\"name\":\"Elizabeth\"}"))
 
-        val result = jsonObject.getString("name")
+        assertEquals("Elizabeth", jsonObject.getString("name"))
+    }
 
-        assertEquals("Elizabeth", result)
+    @Test
+    fun testGetBoolean() {
+        val jsonObject = JsonObject(JSONObject("{\"isActive\":false}"))
+
+        assertFalse(jsonObject.getBoolean("isActive"))
     }
 
     @Test
@@ -34,14 +64,49 @@ class JsonObjectTest {
     }
 
     @Test
-    fun testGetStringArray() {
+    fun testGetBigDecimal() {
+        val jsonObject = JsonObject(JSONObject("{\"id\":12439484359745847}"))
+
+        val result = jsonObject.getBigDecimal("id")
+
+        assertEquals(BigDecimal(12439484359745847), result)
+    }
+
+    @Test
+    fun testGetStringArrayWhenExists() {
         val jsonObject = JsonObject(JSONObject("{\"names\": [\"mark\", \"bob\"]}"))
 
-        val result = jsonObject.getStringArray("names")
+        with(jsonObject.getStringArray("names")) {
+            assertEquals(2, size)
+            assertTrue(contains("mark"))
+            assertTrue(contains("bob"))
+        }
+    }
 
-        assertEquals(2, result.size)
-        assertTrue(result.contains("mark"))
-        assertTrue(result.contains("bob"))
+    @Test
+    fun testGetStringArrayWhenAbsent() {
+        val jsonObject = JsonObject(JSONObject("{\"names\": [\"mark\", \"bob\"]}"))
+
+        assertEquals(0, jsonObject.getStringArray("doesntexist").size)
+    }
+
+    @Test
+    fun testGetArrayWhenExists() {
+        val jsonObject = JsonObject(JSONObject("{'people': [{'name':'george'},{'name':'mike'},{'name':'helen'}]}"))
+
+        with(jsonObject.getArray("people")) {
+            assertEquals(3, size)
+            assertEquals("george", this[0].getString("name"))
+            assertEquals("mike", this[1].getString("name"))
+            assertEquals("helen", this[2].getString("name"))
+        }
+    }
+
+    @Test
+    fun testGetArrayWhenAbsent() {
+        val jsonObject = JsonObject(JSONObject("{'people': [{'name':'george'},{'name':'mike'},{'name':'helen'}]}"))
+
+        assertEquals(0, jsonObject.getArray("doesntexist").size)
     }
 
     @Test
