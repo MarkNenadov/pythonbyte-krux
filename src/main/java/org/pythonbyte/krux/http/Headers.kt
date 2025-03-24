835471a2b0b1f91e5aa683636ab44de4e9239de8
Mark Nenadov <marknenadov@gmail.com>
2025-03-23 23:46:12 -0400

    add a default empty map to buildHeaders

--- src/main/java/org/pythonbyte/krux/http/Headers.kt
+++ src/main/java/org/pythonbyte/krux/http/Headers.kt
@@ -2,7 +2,7 @@ package org.pythonbyte.krux.http
 
 import okhttp3.Headers
 
-fun buildHeaders(headersMap: Map<String, String>): Headers =
+fun buildHeaders(headersMap: Map<String, String> = emptyMap()): Headers =
     Headers.Builder().apply {
         headersMap.forEach { (key, value) -> add(key, value) }
     }.build()
