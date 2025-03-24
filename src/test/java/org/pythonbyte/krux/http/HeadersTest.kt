edb1e988f23668031929b2d9476d8962c3741491
Mark Nenadov <marknenadov@gmail.com>
2025-03-23 23:54:23 -0400

    test case for buildHeaders

---
+++ src/test/java/org/pythonbyte/krux/http/HeadersTest.kt
@@ -0,0 +1,29 @@
+package org.pythonbyte.krux.http
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+
+class HeadersTest {
+    @Test
+    fun testBuildHeadersWhenEmptyHeaders() {
+        with(buildHeaders()) {
+            assertEquals(0, size)
+        }
+    }
+
+    @Test
+    fun testBuildHeadersWhenNotEmptyHeaders() {
+        with(buildHeaders(
+            mapOf(
+                "name" to "Mark",
+                "age" to "88"
+            )
+        )) {
+            assertEquals(2, size)
+            assertTrue(names().contains("name"))
+            assertTrue(names().contains("age"))
+            assertEquals("Mark", get("name"))
+            assertEquals("88", get("age"))
+        }
+    }
+}
